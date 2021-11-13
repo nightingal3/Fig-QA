bbe8e5cd3355c9ed6f52fed6f8b643a554db7fe8
Meng Yan Liu <emmy.liu.1998@gmail.com>
2021-11-13 15:37:47 -0500

    progress on attn viz

--- visualize_attention.py
+++ visualize_attention.py
@@ -57,7 +57,7 @@ def visualize(attention, tokenizer, input_ids, gold=None, normalize=False, save_
 
 def plot_contrastive_attention(model, tokenizer, dataset): 
     for i, metaphor_data in enumerate(dataset):
-        ctx, p1, p2 = metaphor_data["startphrase"], metaphor_data["ending1"], metaphor_data["ending2"]        
+        ctx, p1, p2, label = metaphor_data["startphrase"], metaphor_data["ending1"], metaphor_data["ending2"], metaphor_data["label"]      
         sent1 = ctx + ". " + middle_phrase + p1 + "."
         sent2 = ctx + ". " + middle_phrase + p2 + "."
         if not os.path.isdir(f"./attn_test/{i}"):
@@ -69,18 +69,55 @@ def plot_contrastive_attention(model, tokenizer, dataset):
         _, attn2, input_ids_2 = sent_scoring((model, tokenizer), sent2, use_cuda, score_type, output_attentions=True)
         attn_avg_2 = torch.mean(attn2[-1], dim=1).squeeze(0)
 
-        first_mismatch = np.where(np.array(input_ids_1[0]) != np.array(input_ids_2[0]))[0][0]
-        for j in range(first_mismatch, min(len(attn_avg_1), len(attn_avg_2))):
-            if len(attn_avg_1[j]) != len(attn_avg_2[j]):
-                pdb.set_trace()
-            subtracted_attn = (attn_avg_1[j][:first_mismatch] - attn_avg_2[j][:first_mismatch])
-            visualize(subtracted_attn, tokenizer, input_ids_1[:, :first_mismatch], normalize=False, save_file=f"{i}_test_{j}.png")
+        min_len = min(len(input_ids_1[0]), len(input_ids_2[0]))
+        truncated_1 = input_ids_1[0][:min_len]
+        truncated_2 = input_ids_2[0][:min_len]
+        correct_answer = attn_avg_1 if label == 0 else attn_avg_2
+        wrong_answer = attn_avg_1 if label == 1 else attn_avg_2
+        correct_ids = input_ids_1 if label == 0 else input_ids_2
+        wrong_ids = input_ids_1 if label == 1 else input_ids_2
 
-        if i == 3:
-            assert False
+        correct_words = [tokenizer.decode(i) for i in correct_ids[0]]
+        wrong_words = [tokenizer.decode(i) for i in wrong_ids[0]]
+
+        # fix this for when the sentences are the same length
+        first_mismatch = np.where(np.array(truncated_1) != np.array(truncated_2))[0][0]
+        for j in range(first_mismatch, min(len(attn_avg_1), len(attn_avg_2))):
+            subtracted_attn = (correct_answer[j][:first_mismatch] - wrong_answer[j][:first_mismatch])
+            visualize(subtracted_attn, tokenizer, input_ids_1[:, :first_mismatch], normalize=False, save_file=f"./attn_test/{i}/{j}_correct_{correct_words[j]}_wrong_{wrong_words[j]}.png")
             
 def plot_attention_individual(model, tokenizer, dataset, out_dir):
     pass
+
+# TODO: test token alignment
+# adapted from this notebook: https://colab.research.google.com/github/huggingface/notebooks/blob/master/examples/token_classification.ipynb#scrollTo=GL91xMGP9_CO
+
+def tokenize_and_align_labels(tokenizer, examples):
+    tokenized_inputs = tokenizer(examples["tokens"], truncation=True, is_split_into_words=True)
+
+    labels = []
+    for i, label in enumerate(examples[f"{task}_tags"]):
+        word_ids = tokenized_inputs.word_ids(batch_index=i)
+        previous_word_idx = None
+        label_ids = []
+        for word_idx in word_ids:
+            # Special tokens have a word id that is None. We set the label to -100 so they are automatically
+            # ignored in the loss function.
+            if word_idx is None:
+                label_ids.append(-100)
+            # We set the label for the first token of each word.
+            elif word_idx != previous_word_idx:
+                label_ids.append(label[word_idx])
+            # For the other tokens in a word, we set the label to either the current label or -100, depending on
+            # the label_all_tokens flag.
+            else:
+                label_ids.append(label[word_idx])
+            previous_word_idx = word_idx
+
+        labels.append(label_ids)
+
+    tokenized_inputs["labels"] = labels
+    return tokenized_inputs
 
 if __name__ == "__main__":
     use_cuda = False
@@ -96,6 +133,7 @@ if __name__ == "__main__":
     model, tokenizer = model_init(model_name, use_cuda, output_attentions=True)
 
     plot_contrastive_attention(model, tokenizer, metaphor_set)
+    assert False
 
     for i, metaphor_data in enumerate(metaphor_set):
         ctx, p1, p2 = metaphor_data["startphrase"], metaphor_data["ending1"], metaphor_data["ending2"]        
