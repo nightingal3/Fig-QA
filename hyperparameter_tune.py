19e267635e3007903ecadb89f08b007a3d07c9e2
Emmy Liu <emmy.liu.1998@gmail.com>
2021-11-27 11:23:45 -0500

    add option to tune lambda only

--- hyperparameter_tune.py
+++ hyperparameter_tune.py
@@ -1,6 +1,6 @@
-from ray import tune
-from ray.tune.suggest.hyperopt import HyperOptSearch
-from ray.tune.schedulers import ASHAScheduler
+#from ray import tune
+#from ray.tune.suggest.hyperopt import HyperOptSearch
+#from ray.tune.schedulers import ASHAScheduler
 import argparse
 import pdb
 import pickle
@@ -12,7 +12,6 @@ def optimize(model_name: str, use_cuda: bool, contrastive_train: bool, seed: int
     model_names = {"gpt2": "gpt2", "gpt-neo-sm": "EleutherAI/gpt-neo-1.3B", "gpt-neo-lg": "EleutherAI/gpt-neo-2.7B"}
     model_string = model_names[model_name]
     model, tokenizer = model_init(model_string, use_cuda)
-    batch_size = 8 if model_name == "gpt2" else 2
 
     trainer = training_setup(model, tokenizer, model_string, seed, lr, num_epochs, train_path, eval_path, contrastive_train=contrastive_train, is_hyperparam_opt=True, deepspeed=False, batch_size=batch_size)
 
@@ -39,22 +38,38 @@ def optimize(model_name: str, use_cuda: bool, contrastive_train: bool, seed: int
     return best_trial
 
 def optimize_minimal(model_name: str, use_cuda: bool, contrastive_train: bool, seed: int, train_path: str, eval_path: str) -> tuple:
-    learning_rates = [1e-5 * i for i in range(1, 5)]
-    epochs = range(3, 6)
-    batch_size = 8 if model_name == "gpt2" else 2
+    if contrastive_train:
+        lambd_lst = [0.1 * i for i in range(1, 11)]
+        batch_size = 2
 
-    trials = {}
-    best_eval_loss = float("inf")
-    best_trial = None
+        trials = {}
+        best_eval_loss = float("inf")
+        best_trial = None
 
-    for lr, num_epochs in zip(learning_rates, epochs):
-        #trainer = training_setup(model, tokenizer, model_string, seed, lr, num_epochs, train_path, contrastive_train=contrastive_train, is_hyperparam_opt=False)
-        results = main(model_name, "", train_path, eval_path, contrastive_train, num_epochs, seed, lr, use_cuda, dont_train=False, dont_eval=False, out_path=None, batch_size=batch_size)
-        if results["eval_loss"] < best_eval_loss:
-            best_eval_loss = results["eval_loss"]
-            best_trial = {"lr": lr, "num_epochs": num_epochs}
-        trials[f"lr_{lr}_epochs_{num_epochs}"] = results["eval_loss"]
-    
+        for lambd in lambd_lst:
+            #trainer = training_setup(model, tokenizer, model_string, seed, lr, num_epochs, train_path, contrastive_train=contrastive_train, is_hyperparam_opt=False)
+            results = main(model_name, "", train_path, eval_path, contrastive_train, lambd, 3, seed, 5e-5, use_cuda, dont_train=False, dont_eval=False, out_path=None, batch_size=batch_size)
+            if results["eval_loss"] < best_eval_loss:
+                best_eval_loss = results["eval_loss"]
+                best_trial = {"lambd": lambd}
+            trials[f"contrast_lambd_{lambd}"] = results["eval_loss"]
+    else:
+        learning_rates = [1e-5 * i for i in range(1, 5)]
+        epochs = range(3, 6)
+        batch_size = 8
+
+        trials = {}
+        best_eval_loss = float("inf")
+        best_trial = None
+
+        for lr, num_epochs in zip(learning_rates, epochs):
+            #trainer = training_setup(model, tokenizer, model_string, seed, lr, num_epochs, train_path, contrastive_train=contrastive_train, is_hyperparam_opt=False)
+            results = main(model_name, "", train_path, eval_path, contrastive_train, 1, num_epochs, seed, lr, use_cuda, dont_train=False, dont_eval=False, out_path=None, batch_size=batch_size)
+            if results["eval_loss"] < best_eval_loss:
+                best_eval_loss = results["eval_loss"]
+                best_trial = {"lr": lr, "num_epochs": num_epochs}
+            trials[f"lr_{lr}_epochs_{num_epochs}"] = results["eval_loss"]
+        
     return best_trial, trials
 
 
@@ -72,7 +87,8 @@ if __name__ == "__main__":
     parser.add_argument("-e", "--eval_path", default="./lm_train_data/dev.txt")
 
     args = parser.parse_args()
-    
+    if args.contrastive:
+        best_trial, all_trials = optimize_minimal(args.model, args.cuda, True, args.seed, args.train_path, args.eval_path)
     if args.model == "gpt2": 
         best_trial = optimize(args.model, args.cuda, args.contrastive, args.seed, args.lr, args.num_epochs, args.train_path, args.eval_path)
         all_trials = {}
