4e82ff98714c64aec0548dd5c17ea7d52abd960d
Emmy Liu <emmy.liu.1998@gmail.com>
2021-12-07 20:33:22 -0500

    lost bar chart code?

--- make_performance_bar_chart.py
+++ make_performance_bar_chart.py
@@ -1,21 +1,41 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import pdb
 
 sns.set_theme(style="whitegrid")
+sns.set(font_scale = 1.2)
+def make_plot(df, out_filepath: str, palette: str = "dark", plot_type: str = "finetune") -> None: # from this example: https://seaborn.pydata.org/examples/grouped_barplot.html
+    if plot_type == "finetune":
+        order = ["GPT-2", "GPT-neo 1.3B", "GPT-3 Ada", "GPT-3 Babbage", "GPT-3 Curie", "GPT-3 Davinci", "BERT", "RoBERTa", "Human", "Human (confident)"]
+    elif plot_type == "prompt":
+        order = ["GPT-2", "GPT-neo 1.3B", "GPT-3 Ada", "GPT-3 Davinci"]
+    elif plot_type == "transfer":
+        order = ["BERT", "RoBERTa"]
 
-def make_plot(df, out_filepath: str) -> None: # from this example: https://seaborn.pydata.org/examples/grouped_barplot.html
     g = sns.catplot(
     data=df, kind="bar",
     x="Model", y="Accuracy", hue="Method",
-    ci="sd", palette="dark", alpha=.6, height=6
+    ci="sd", palette=palette, height=6, order=order
     )
     g.despine(left=True)
     g.set_axis_labels("Method", "Accuracy")
-    g.legend.set_title("")
+    g._legend.remove()
+
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05),
+          ncol=3, fancybox=True, shadow=True)
+    plt.ylim(0.5, 1)
+    plt.xticks(rotation=90)
+    plt.tight_layout()
     plt.savefig(f"{out_filepath}.png")
     plt.savefig(f"{out_filepath}.eps")
 
 if __name__ == "__main__":
     df = pd.read_csv("./metaphor_performance_results.csv")
-    make_plot(df, "./performance_barchart")
+    df_zero_shot_finetune = df.loc[((df["Method"] == 'Zero-shot') | (df["Method"] == "Fine-tuning")) | (df["Method"] == "Human")]
+    df_prompting = df[df["Method"].str.contains("Prompt")]
+    df_transfer = df[df["Method"].str.contains("Transfer")]
+
+    make_plot(df_zero_shot_finetune, "performance_barchart_zeroshot_finetune")
+    make_plot(df_prompting, "prompting_bar", "rocket", plot_type = "prompt")
+    make_plot(df_transfer, "transfer_bar", "crest", plot_type = "transfer")
